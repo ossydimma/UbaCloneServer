c2847f02aa96da169906d8fc207623aa073ac4cb
Osita <chrisjerry070@gmail.com>
2024-11-05 21:15:44 +0400

    first commit

--- UbaClone.WebApi/obj/Release/net8.0/UbaClone.WebApi.AssemblyInfo.cs
+++ UbaClone.WebApi/obj/Release/net8.0/UbaClone.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UbaClone.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68250319e0491931b62af1c69cf4cd1d6b5d0245")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1380428df5430967b31646227e5907b4a9fca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UbaClone.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UbaClone.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
